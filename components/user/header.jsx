80bccabe36d3baad44967076cb23c1b7ce0c8813
Furkan <furkancosar2005@gmail.com>
2024-10-17 15:32:33 +0300

    log out eklendi

--- components/user/header.jsx
+++ components/user/header.jsx
@@ -1,7 +1,21 @@
+import { signOut } from "@/app/login/actions";
+import { createClient } from "@/utils/supabase/server";
+
 export default async function Header() {
+  const supabase = createClient();
+  const { data: {user} } = await supabase.auth.getUser();
+
   return (
-    <header>
-        <h1>header</h1>
+    <header className="header">
+        <h1>Medium</h1>
+
+        <form action="">
+          <button className="write-button">Write</button>
+        </form>
+
+        <form action={signOut}>
+          <button>Log Out</button>
+        </form>
     </header>
   )
 }
